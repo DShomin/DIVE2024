a3176b38add521dfc9c7171b13f636e9bfcb25f5
DShomin <lhm53779354@gmail.com>
2024-09-30 12:35:26 +0900

    add analysis description prompt

--- langgraph_back/dive_agent/AnalysisAgent.py
+++ langgraph_back/dive_agent/AnalysisAgent.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
 
+from typing import Dict
 from dive_agent.DatabaseManager import DatabaseManager
 from dive_agent.LLMManager import LLMManager
+from dive_agent.Analyses import correlation
 
 
 class AnalysisAgent:
     def __init__(self):
         self.llm_manager = LLMManager()
         self.db_manager = DatabaseManager()
+
+        self.analysis_tools = {"correlation": correlation.BASE_DESCRIPTION}
+
+    def get_analysis_tools(self) -> Dict[str, str]:
+
+        return self.analysis_tools
